c286877f21708cef322876995893d762c41bd39a
yasza <86324593+yaszaxz@users.noreply.github.com>
2026-01-14 13:16:10 -0300

    Verifica se o nome contém caracteres que não são letras ou números no caso da calculadora

--- Nível 1/02-QualOSeuNome.py
+++ Nível 1/02-QualOSeuNome.py
@@ -1,6 +1,14 @@
 #Perguntar o nome do usuário
 #Digitar o nome
 #Exibir a mensagem
-print('Digite o seu nome')
-nome = input() 
+
+while True:
+    nome = input('Digite o seu nome: ').strip()
+    nomeLimpo = nome.replace(' ', '').replace('-', '')
+    
+    if nomeLimpo.isalpha():
+        break
+    else:
+        print('Nome inválido, digite apenas letras')
+
 print('Olá,',nome)
--- Nível 1/03-QuaseUmaCalculadora.py
+++ Nível 1/03-QuaseUmaCalculadora.py
@@ -1,18 +1,25 @@
-#Pedir o número 1
-#Pedir o número 2
-#Realizar as operações
-#Exibir na tela
+def lerNumero(mensagem):
+    while True:      
+        try:
+            return float(input(mensagem))
+        except ValueError:
+            print('Valor inválido, digite apenas NÚMEROS')
+
 print('Bem-vindo à sua quase calculadora!')
-#Para definir uma variável de um tipo específico nessa situação, variavel = tipo(input('coisas'))
-numero1 = float(input('Digite um número: '))
-numero2 = float(input('Digite mais outro número: '))
+
+numero1 = lerNumero('Digite um número: ')
+numero2 = lerNumero('Digite um segundo número: ')
 
 soma = numero1 + numero2
 diferenca = numero1 - numero2
 multiplicacao = numero1 * numero2
-divisao = numero1 / numero2
 
 print('Soma:', soma)
-print('Diferença', diferenca)
-print('Multiplicação', multiplicacao)
-print('Divisão', divisao)
+print('Diferença:', diferenca)
+print('Multiplicação:', multiplicacao)
+
+if(numero2 != 0):
+    divisao = numero1 / numero2
+    print('Divisão:', divisao)
+else:
+    print('Divisão por zero não existe')
